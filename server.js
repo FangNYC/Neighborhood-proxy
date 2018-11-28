5e7d4c07a7f14591f4861cb986412698dbe97770
Kenny <kennethpolyak@gmail.com>
2018-11-28 16:50:36 -0500

    Setup route for neighborhood service ssr

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('newrelic');
+
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -19,9 +21,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Neighborhood API endpoints
 app.get('/listingdata', (req, res) => {
   let requestId = req.query.id;
-  console.log('REQ id TO listingdata api', requestId);
   requestId = requestId.slice(-3) * 1;
-  axios.get(`http://localhost:3001/listingdata?id=${requestId}`)
+  axios.get(`http://localhost:3001/api/listingdata?id=${requestId}`)
   .then((results) => res.send(results.data))
   .catch((err) => console.error(err));
 })
@@ -29,7 +30,7 @@ app.get('/listingdata', (req, res) => {
 app.get('/neighborhooddata', (req, res) => {
   let requestId = req.query.id;
   requestId = requestId.slice(-3) * 1;
-  axios.get(`http://localhost:3001/neighborhooddata?id=${requestId}`)
+  axios.get(`http://localhost:3001/api/neighborhooddata?id=${requestId}`)
   .then((results) => res.send(results.data))
   .catch((err) => console.error(err));
 })
@@ -37,7 +38,7 @@ app.get('/neighborhooddata', (req, res) => {
 app.get('/landmarkdata', (req, res) => {
   let lat = req.query.listingLat;
   let long = req.query.listingLong;
-  axios.get(`http://localhost/landmarkdata?listingLat=${lat}&listingLong=${long}`)
+  axios.get(`http://localhost:3001/api/landmarkdata?listingLat=${lat}&listingLong=${long}`)
   .then((results) => res.send(results.data))
   .catch((err) => console.error(err));
 })
@@ -103,9 +104,59 @@ app.get('/landmarkdata', (req, res) => {
 //   });
 // })
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// })
+
+app.get('/listing', (req, res) => {
+  Promise.all([
+    axios.get('http://localhost:3001/renderNeighborhood', {
+      params: {
+        id: req.query.id
+      }
+    })
+  ])
+  .then((results) => {
+    let strings = [];
+    let props = [];
+    results.forEach(({data}) => {
+      strings.push(data[0]);
+      props.push(data[1]);
+    });
+    res.end(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Staybnb</title>
+        <!-- <link rel="stylesheet" type="text/css" href="https://localhost:3001/styles.css"> -->
+      </head>
+      <body>
+        <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+        <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/styles.css">
+        <script src="http://localhost:3001/app.js"></script>
+      
+        <div id="description"></div>
+        <div class="container-left">
+            <div id="reviews"></div>
+            <div id="neighborhood">${strings[0]}</div>
+        </div>
+        <div class=container-right>
+          <div id="booking"></div>
+        </div>
+        <script>
+          ReactDOM.hydrate(
+            React.createElement(Neighborhood, ${props[0]}),
+            document.getElementById('neighborhood')
+          );
+        </script>
+      </body>
+      </html>
+    `)
+  })
+});
+
 
 // app.get('/rooms/:id', function(req, res) {
 //   const id = req.params.id;
